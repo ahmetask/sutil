3df16918e302768cc40ab04235324c4552a75079
Ahmet Aşık <ahmet.asik@trendyol.com>
2021-06-15 16:17:07 +0300

    Null util added

--- struct.go
+++ struct.go
@@ -1,14 +1,22 @@
 package sutil
 
+import (
+	"errors"
+	"reflect"
+	"strings"
+)
+
 type path struct {
 	json  bool
 	value string
 }
 
 type SUtil struct {
-	data  interface{}
-	value interface{}
-	path  path
+	data    interface{}
+	value   interface{}
+	path    path
+	success bool
+	err     error
 }
 
 type ISUtil interface {
@@ -22,6 +30,7 @@ func New(data interface{}) ISUtil {
 		data: data,
 	}
 }
+
 func (s *SUtil) WithValue(v interface{}) ISUtil {
 	s.value = v
 	return s
@@ -32,8 +41,95 @@ func (s *SUtil) WithPath(p string, json bool) ISUtil {
 	return s
 }
 
+func (s *SUtil) reflectValue(i interface{}) reflect.Value {
+	v := reflect.ValueOf(i)
+
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	return v
+}
+
+func (s *SUtil) result(err error) {
+	if err != nil {
+		s.success = false
+		s.err = err
+	} else {
+		s.success = true
+		s.err = nil
+	}
+
+}
+func (s *SUtil) set(r reflect.Value, v interface{}) {
+	if !r.IsValid() {
+		s.result(errors.New("invalid value"))
+		return
+	}
+	if r.CanSet() {
+		if v == nil {
+			r.Set(reflect.Zero(r.Type()))
+			s.result(nil)
+			return
+		}
+		vPrime := s.reflectValue(v)
+		if r.Kind() == vPrime.Kind() {
+			r.Set(vPrime)
+			s.result(nil)
+		}
+	} else {
+		s.result(errors.New("value can not be set"))
+	}
+}
+
+func (s *SUtil) setR(o interface{}, v interface{}, base string) {
+	rv := s.reflectValue(o)
+
+	if rv.Kind() == reflect.Invalid {
+		return
+	} else if rv.Kind() != reflect.Struct {
+		if s.path.value == strings.TrimPrefix(base, ".") {
+			s.set(rv, v)
+		}
+		return
+	}
+
+	reflectType := rv.Type()
+
+	for i := 0; i < reflectType.NumField(); i++ {
+		field := reflectType.Field(i)
+
+		p := ""
+		if s.path.json {
+			p = base + "." + field.Tag.Get("json")
+		} else {
+			p = base + "." + field.Name
+		}
+
+		fieldI := rv.Field(i)
+		pathMatch := s.path.value == strings.TrimPrefix(p, ".")
+		if fieldI.Kind() == reflect.Struct || fieldI.Kind() == reflect.Ptr {
+			if pathMatch {
+				if fieldI.CanSet() {
+					if fieldI.Kind() == reflect.Ptr && v != nil {
+						fieldI = fieldI.Elem()
+					}
+					s.set(fieldI, v)
+				}
+			} else {
+				s.setR(fieldI.Interface(), v, p)
+			}
+		} else {
+			if pathMatch {
+				s.set(fieldI, v)
+			}
+		}
+	}
+
+	return
+}
+
 func (s *SUtil) Set() (bool, error) {
-
-
-	return true, nil
+	s.setR(s.data, s.value, "")
+	return s.success, s.err
 }
---
+++ struct_test.go
@@ -0,0 +1,181 @@
+package sutil
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type DeepStruct struct {
+	C *string `json:"c"`
+	F string  `json:"f"`
+	D int64   `json:"d"`
+}
+
+type InnerStruct struct {
+	B *int64      `json:"b"`
+	D *DeepStruct `json:"d"`
+}
+
+type MyStruct struct {
+	A string       `json:"a"`
+	I *InnerStruct `json:"i"`
+}
+
+type Int64 *int64
+type String *string
+
+func NewInt64(value int64) Int64 {
+	return &value
+}
+func NewString(value string) String {
+	return &value
+}
+
+func TestSetIsNil(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("a")}}}
+
+	r, err := New(v).WithValue(nil).WithPath("I.D.C", false).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Nil(t, v.I.D.C)
+}
+
+func TestSet(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("A")}}}
+
+	r, err := New(v).WithValue(NewString("test")).WithPath("I.D.C", false).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, *v.I.D.C, *NewString("test"))
+}
+
+func TestSet_json(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("A")}}}
+
+	r, err := New(v).WithValue(NewString("test")).WithPath("i.d.c", true).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, *v.I.D.C, *NewString("test"))
+}
+
+func TestSet_Without_Addr(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("A"), D: 1}}}
+
+	r, err := New(v).WithValue(NewString("test")).WithPath("I.D.D", false).Set()
+
+	assert.Nil(t, err)
+	assert.False(t, r)
+	assert.Equal(t, v.I.D.D, int64(1))
+}
+
+func TestSet_Without_Addr_json(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("A"), D: 1}}}
+
+	r, err := New(v).WithValue(NewString("test")).WithPath("i.d.d", true).Set()
+
+	assert.Nil(t, err)
+	assert.False(t, r)
+	assert.Equal(t, v.I.D.D, int64(1))
+}
+
+func TestSet_Struct(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2)}}
+
+	r, err := New(v).WithValue(&InnerStruct{B: NewInt64(3)}).WithPath("I", false).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, *v.I.B, int64(3))
+}
+
+func TestSet_Struct_json(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2)}}
+
+	r, err := New(v).WithValue(&InnerStruct{B: NewInt64(3)}).WithPath("i", true).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, *v.I.B, int64(3))
+}
+
+func TestSet_High_Level(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2)}}
+
+	r, err := New(v).WithValue("test").WithPath("A", false).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, v.A, "test")
+}
+
+func TestSet_High_Level_json(t *testing.T) {
+
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2)}}
+
+	r, err := New(v).WithValue("test").WithPath("a", true).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, v.A, "test")
+}
+
+func TestSet_Low_Level_String(t *testing.T) {
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("A"), D: 1, F: "Foo"}}}
+
+	r, err := New(v).WithValue("test").WithPath("I.D.F", false).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, v.I.D.F, "test")
+}
+
+func TestSet_Low_Level_String_Json(t *testing.T) {
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("A"), D: 1, F: "Foo"}}}
+
+	r, err := New(v).WithValue("test").WithPath("i.d.f", true).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, v.I.D.F, "test")
+}
+
+func TestSet_WithoutStruct(t *testing.T) {
+	v := NewInt64(1)
+
+	r, err := New(v).WithValue(NewInt64(2)).WithPath("", false).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Equal(t, *v, int64(2))
+}
+
+func TestSet_With_Nil(t *testing.T) {
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("A"), D: 1, F: "Foo"}}}
+
+	r, err := New(v).WithValue(nil).WithPath("I.B", false).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Nil(t, v.I.B)
+}
+
+func TestSet_With_Nil_Json(t *testing.T) {
+	v := &MyStruct{A: "Foo", I: &InnerStruct{B: NewInt64(2), D: &DeepStruct{C: NewString("A"), D: 1, F: "Foo"}}}
+
+	r, err := New(v).WithValue(nil).WithPath("i.b", true).Set()
+
+	assert.Nil(t, err)
+	assert.True(t, r)
+	assert.Nil(t, v.I.B)
+}
